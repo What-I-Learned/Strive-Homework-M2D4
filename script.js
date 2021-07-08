91c038d709d02d62e9896822a060ffec7664f6b0
js25s <petrauskas.justas@gmail.com>
2021-07-08 19:54:17 +0200

    not working

--- script.js
+++ script.js
@@ -2,58 +2,142 @@ window.onload = function(){
     displayandGetAmount()
 }
 
-    const range = document.getElementById("amountOfGroups").oninput = function(){
-        displayandGetAmount();
 
-    }
-
-
+const range = document.getElementById("amountOfGroups")
+range.addEventListener("input",displayandGetAmount)
+        
 
 
-    function displayandGetAmount(){
+
+
+
+function displayandGetAmount(){
         let number = document.getElementById("amountOfGroups").value
         const numberOfTeams = document.getElementById("numberOfTeams")
         numberOfTeams.innerText = number;
         return +number
+}
+
+
+const addBtn = document.querySelector(".addBtn")
+const nameInput = document.getElementById("name")
+
+addBtn.addEventListener("click",addStudentToList)
+nameInput.addEventListener("keypress",function(e){
+    if (e.key === 'Enter' && nameInput.value != ''){
+        addStudentToList()
     }
+})
 
-
-    const addBtn = document.querySelector(".addBtn")
-
-    addBtn.addEventListener("click",addStudentToList)
-
-    function addStudentToList(){
-        const name = document.getElementById("name")
+function addStudentToList(){
+        
         const studentList = document.getElementById("list-of-students");
         let classesToAdd = ["list-group-item","rounded","newStudent"]
 
         const newStudent = document.createElement("li")
-        newStudent.innerText = name.value
         newStudent.classList.add(...classesToAdd)
+        newStudent.innerText = nameInput.value
         studentList.appendChild(newStudent)
-        
-        if(name.value === ''){
-            return null
-        }
+        nameInput.value = ''
+
+
 
     }
+
+
+function generateTeamContainers(){
+
+        const teams = document.getElementById("teams")
+        //-- amount of teams
+        let amountOfTeams =displayandGetAmount()
+
+        let divClassesToAdd = ["col-xl-3","col-md-4","col-sm-6","team-card"]
+        let subDivClassesToAdd = ["card","shadow-sm"]
+        let carClassToAdd = "card-header"
+
+        // let ulClassesToAdd = ["list-group","list-group-flush"]
+        // let liClassesToAdd = "list-group-item"
+
+        for(let i=0;i<amountOfTeams;i++){
+            const newTeam = document.createElement("div");
+            const newTeamCard = document.createElement("div");
+            const newCardHeader = document.createElement("div")
+            newTeam.classList.add(...divClassesToAdd)
+            newTeamCard.classList.add(...subDivClassesToAdd)
+            newCardHeader.classList.add("card-header")
+            newCardHeader.innerText = `Team ${i+1}`
+            teams.appendChild(newTeam)
+            newTeam.appendChild(newTeamCard)
+            newTeamCard.appendChild(newCardHeader)
+        }
+
+}
+//generateTeamContainers()
 
 const assignTeams = document.getElementById("assigToTeams")
+assignTeams.addEventListener("click",function(){
+    displayandGetAmount()
+    generateTeamContainers()
+    generateStudentArray()
+    splitIntoTeams()
+})
 
-assignTeams.addEventListener("click",assignStudentsToTeams)
 
-    function assignStudentsToTeams(){
+
+function generateStudentArray(){
         const studentList = document.querySelectorAll(".newStudent");
         const studentArray = []
-
         if(studentList.length !=0){
             for(let student of studentList){
-                studentArray.push(student.nodeValue)
+                studentArray.push(student.innerText)
             }
+        }else{
+            console.log("Empty List");
         }
-
-        //-- add to student array
-        console.log(studentList);
+        return studentArray
     
+}
+
+
+//-- everything should happend with the button click
+function splitIntoTeams(){
+    let studentArray = generateStudentArray();
+    let numberOfTeams = displayandGetAmount();
+    let amountOfStudents = studentArray.length
+    
+    let teams = []
+    if(studentArray.length>numberOfTeams){
+    for(let i=0;i<numberOfTeams;i++){
+        teams[i]=[];
+        for(let b=0;b<=amountOfStudents;b++){
+            let getRandomStudent = Math.floor(Math.random()*amountOfStudents)
+           if(teams[i].includes(studentArray[getRandomStudent])){
+               teams[i].splice(studentArray[getRandomStudent],1)
+            }else{
+                console.log("false");
+                teams[i].push(studentArray[getRandomStudent])
+                studentArray.splice(studentArray[getRandomStudent],1)
+                amountOfStudents--;
+            }
+                
+                console.log(studentArray);
+
+           
+            // if(teams[i].indexOf(studentArray[getRandomStudent])==-1){
+            //     
+            // }
+                
+            //     
+            //     
+            //     console.log(studentArray.splice(studentArray[getRandomStudent],1));
+            
+                
+
+        }
     }
-    assignStudentsToTeams()
+    }
+
+
+    console.log(teams);
+}
+
